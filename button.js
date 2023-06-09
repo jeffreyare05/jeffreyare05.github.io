4deaf889b1d7cf83bed76ee0ea93bca2f48aa95b
jeffreyare05 <135644772+jeffreyare05@users.noreply.github.com>
2023-06-09 02:03:10 -0400

    Update button.js

--- button.js
+++ button.js
@@ -4,7 +4,7 @@ function changeText()
   Var id=document.getElementsByName("button")[0].id;
   if(id==1)
   {
-    document.getElementByID("text").innerHTML = "I hope you had fun but im happy you're coming back I wanna give you a hug lol"; 
+    document.getElementById("text").innerHTML = "I hope you had fun but im happy you're coming back I wanna give you a hug lol"; 
     document.getElementsByName("button")[0].id=0;
   }
 }
