add7fa6f927c383dd7fd9b9f2268c0ea4e559f71
jeffreyare05 <135644772+jeffreyare05@users.noreply.github.com>
2023-06-09 01:58:55 -0400

    Create button.js

---
+++ button.js
@@ -0,0 +1,10 @@
+function changeText()
+{
+  
+  Var id=document.getElementsByName("button")[0].id;
+  if(id==1)
+  {
+    document.getElementByID("text").innerHTML = "I hope you had fun but im happy you're coming back I wanna give you a hug lol"; 
+    document.getElementsByName("button")[0].id=0;
+  }
+}
